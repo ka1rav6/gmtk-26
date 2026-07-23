4a2083ccdb98379d846e7abd6673a317d59eea52
me <66961093+EvilCultist@users.noreply.github.com>
2026-07-23 20:46:32 +0530

    update temp

--- scenes/characters/enemies/basic_enemy.gd
+++ scenes/characters/enemies/basic_enemy.gd
@@ -1,26 +1,22 @@
 extends "res://scenes/characters/generic_enemy.gd"
 
 
-func toggle_sprite() -> void:
-	$select_sprite.visible = Global.powerMode
-
 func _physics_process(delta: float) -> void:
-	var tDelta = delta * currentTimeFactor
 	var tSpeed = SPEED * currentTimeFactor
+	var tMaxVel = MAX_VELOCITY * currentTimeFactor
 	var tJumpVelocity = JUMP_VELOCITY * sqrt(currentTimeFactor)
-	if not is_on_floor():
-		velocity += get_gravity() * tDelta 
 	
 	if is_on_wall() and is_on_floor():
-		# velocity.y = JUMP_VELOCITY
 		velocity.y = tJumpVelocity
 	
 	if Global.player:
 		var direction :=  1 if ((Global.player.global_position.x - global_position.x) > 0) else -1
 		if direction:
-			velocity.x = direction * tSpeed
+			velocity.x += direction * tSpeed
 		else:
-			velocity.x = move_toward(velocity.x, 0, tSpeed)
-	#velocity *= currentTimeFactor
+			velocity.x += move_toward(velocity.x, 0, tSpeed)
+		if velocity.x > tMaxVel:
+			velocity.x = sign(velocity.x) * tMaxVel
 	
-	move_and_slide()
+	# move and slide is called by super
+	super._physics_process(delta)
--- scenes/characters/generic_enemy.gd
+++ scenes/characters/generic_enemy.gd
@@ -1,21 +1,41 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 200.0
-const JUMP_VELOCITY = -400.0
-
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = -400.0
 @export var MAX_HEALTH := 25
-var health: int
+@export var MAX_VELOCITY := 80.0
+@export var COUNTDOWN_HEIGHT := 60
+@export var onHoverScaleFactor = 1.25
+@export var slowDownTimeFactor = 0.1
+@export var power := 8.0
+@export var max_drag := 200.0
+@export var line : Line2D = null
+@export var sels : Sprite2D = null
+@export var mc : Area2D = null
 
-@export var COUNTDOWN_HEIGHT := 25
+@onready var tmrScene: PackedScene = preload("res://scenes/characters/timer_node.tscn")
+@onready var health:= MAX_HEALTH
 
-var tmrScene: PackedScene
-
-@onready var currentTimeFactor := 1.0
+var currentTimeFactor := 1.0
+var isAffectedBy := 0
+var dragging := false
+var drag_start := Vector2.ZERO
+var drag_current := Vector2.ZERO
 
 func _ready() -> void:
-	tmrScene = preload("res://scenes/characters/timer_node.tscn")
-	health = MAX_HEALTH
+	if line == null:
+		line = $ShootDir
+	if sels == null:
+		sels = $select_sprite
+	if mc == null:
+		mc = $mouseCollider
+	mc.input_event.connect(_on_mouse_collider_input_event)
+	mc.mouse_entered.connect(_on_mouse_collider_mouse_entered)
+	mc.mouse_exited.connect(_on_mouse_collider_mouse_exited)
+
+func toggle_sprite() -> void:
+	sels.visible = Global.powerMode
 
 func CreateTimer(time: int, functi: Callable):
 	var x = tmrScene.instantiate()
@@ -27,3 +47,66 @@ func CreateTimer(time: int, functi: Callable):
 
 func set_speed(mult: float) -> void:
 	currentTimeFactor *= mult
+
+func _on_mouse_collider_mouse_entered() -> void:
+	sels.scale *= onHoverScaleFactor
+
+func _on_mouse_collider_mouse_exited() -> void:
+	sels.scale /= onHoverScaleFactor
+
+func _tf() -> void:
+	currentTimeFactor /= slowDownTimeFactor
+	isAffectedBy -= 1
+
+func shoot() -> void:
+	var drag = global_position - get_global_mouse_position()
+	if drag.length() > max_drag:
+		drag = drag.normalized() * max_drag
+	velocity += drag * power
+
+func update_trajectory() -> void:
+	line.clear_points()
+	var drag = global_position - get_global_mouse_position()
+	if drag.length() > max_drag:
+		drag = drag.normalized() * max_drag
+	var vel2 = velocity + drag * power
+	
+	var simpos = Vector2.ZERO
+	#var simvel = vel2
+	var step_dt = 0.08
+	
+	for i in range(30):
+		line.add_point(simpos)
+		vel2 += get_gravity() * currentTimeFactor * step_dt / Global.ULTRAINSTINCT_SLOWDOWN
+		simpos += vel2 * step_dt
+	pass
+
+func _on_mouse_collider_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if not Global.powerMode:
+		return
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			Global.toggle_all()
+			currentTimeFactor *= slowDownTimeFactor
+			isAffectedBy += 1
+			CreateTimer(5, Callable(self, "_tf"))
+		elif event.button_index == MOUSE_BUTTON_RIGHT and isAffectedBy > 0:
+			if event.is_released() and dragging:
+				Global.toggle_all()
+				shoot()
+				mc.scale /= 20.0
+				dragging = false
+				line.clear_points()
+			elif event.pressed:
+				if global_position.distance_squared_to(get_global_mouse_position()) < 576:
+					mc.scale *= 20.0
+					dragging = true
+	elif event is InputEventMouseMotion and dragging:
+		update_trajectory()
+
+func _physics_process(delta: float) -> void:
+	var tDelta = delta * currentTimeFactor
+	if not is_on_floor():
+		velocity += get_gravity() * tDelta 
+	
+	move_and_slide()
