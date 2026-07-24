e7643f08920a367461b5425a09e5e4793b03fb7e
me <66961093+EvilCultist@users.noreply.github.com>
2026-07-24 22:16:15 +0530

    typo

--- scenes/characters/enemies/sniperEnemy.gd
+++ scenes/characters/enemies/sniperEnemy.gd
@@ -3,6 +3,7 @@ extends "res://scenes/characters/generic_enemy.gd"
 @onready var motionRayScene: PackedScene = preload("res://scenes/effects/motion_ray.tscn")
 @onready var animP: AnimationPlayer = $AnimationPlayer
 @onready var crossHair: Sprite2D = $crosshair
+@onready var sight: Line2D = $sight
 @onready var playerPos:= Vector2.ZERO
 var isShooting := false
 
@@ -19,12 +20,15 @@ func set_speed(mult: float) -> void:
 func _schedule_pulse() -> void:
 	if (not is_instance_valid(Global.player)):
 		return
-	if (not isShooting) && (Global.player.global_position - global_position).length_squared() < 100000.0:
+	if (not isShooting) && (Global.player.global_position - global_position).length_squared() < 10000000.0:
 		isShooting = true
 		playerPos = Global.player.global_position
 		crossHair.reparent(get_parent())
 		crossHair.global_position = playerPos
 		crossHair.visible = true
+		sight.clear_points()
+		sight.add_point(Vector2.ZERO)
+		sight.add_point(to_local(crossHair.global_position))
 		animP.play("shoot", 0.5, randf_range(0.75, 1.25))
 
 
@@ -35,6 +39,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, tSpeed)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 2 * tSpeed)
+	if velocity != Vector2.ZERO or !is_on_floor():
+		sight.clear_points()
+		sight.add_point(Vector2.ZERO)
+		sight.add_point(to_local(crossHair.global_position))
+		
 	super._physics_process(delta)
 
 func pulse() -> void:
@@ -47,6 +56,7 @@ func pulse() -> void:
 		return
 	var dir = (playerPos - global_position).normalized()
 	w.fire(dir, [get_rid()] as Array[RID])
+	sight.clear_points()
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
