09920255dbf6f0f83ed88b2ecaf23712495590d3
me <66961093+EvilCultist@users.noreply.github.com>
2026-07-26 19:43:06 +0530

    Glue. Match sticks. Paper. Build script!

--- scenes/level_elements/portal.gd
+++ scenes/level_elements/portal.gd
@@ -5,3 +5,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body == Global.player:
 		get_tree().call_deferred("change_scene_to_packed", nextLevel)
+		Global.setup_level_timer_if_needed.call_deferred()
